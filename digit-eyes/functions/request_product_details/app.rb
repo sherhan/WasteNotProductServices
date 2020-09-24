543fca1de1782a22ec21a6bb4e5651fe3d5bf162
Sherhan Wicky <sherhanwicky@192-168-1-105.tpgi.com.au>
2020-09-25 08:33:18 +1000

    Refactor lambda to simplify code
    Move generate_signature, create_query_params functions into
    
    separate file.

--- digit-eyes/functions/request_product_details/app.rb
+++ digit-eyes/functions/request_product_details/app.rb
@@ -3,32 +3,11 @@ require 'base64'
 require 'hmac-sha1'
 require 'net/http'
 
-# URI
-BASE_URI = 'https://www.digit-eyes.com/gtin/v2_0/'
-LANG = "en"
-
-# generates a signature for digit-eyes api calls
-# Returns: Base64 encoded HMAC-SHA1 hash of *key + UPC/EAN code)
-def generate_signature(key, upc_code)
-
-    hmac = (HMAC::SHA1.new(key) << upc_code).digest
-    return  Base64.strict_encode64(hmac)
-    
-end
-
-# create query params hashtable for the digit-eye api without app_key included
-def create_query_params_keyless(app_key, upc_code, field_names, language)
-    return { 
-      "upcCode" => upc_code, 
-      "field_names" => "all", 
-      "language" => "en",
-      "signature" => generate_signature(app_key, upc_code)
-    }
-  end  
+require_relative '../utils/digiteyes_utils.rb'
 
 def lookup_upc(app_key, upc_code, field_names, language)
     # Create URI
-    uri = ENV['BASE_URI']
+    uri = URI(ENV["BASE_URI"])
     query_params = create_query_params_keyless(app_key, upc_code, field_names, language)
     uri.query = URI.encode_www_form(query_params)
 
@@ -38,12 +17,12 @@ end
 
 def lambda_handler(event:, context:) 
     # Initialise inputs
-    app_key = ENV['DIGITEYES_KEY']
-    lang = ENV['LANGUAGE']
+    app_key = ENV["DIGITEYES_KEY"]
+    lang = ENV["LANGUAGE"]
     upc_code = "9310022130908"
     field_names = "all"
 
     return lookup_upc(app_key, upc_code, field_names, lang)
 end
 
-#puts lambda_handler(event: '', context: '')
+# puts lambda_handler(event: '', context: '')
