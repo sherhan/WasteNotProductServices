d6cd620b5002cde7cfb2ebb679028ec8f2c172ee
Sherhan Wicky <sherhanwicky@192-168-1-105.tpgi.com.au>
2020-09-25 12:49:23 +1000

    Update test script
    - accept command line args for running individual tests
    - add start api test to allow testing of API gateway integration

--- digit-eyes/run_tests.sh
+++ digit-eyes/run_tests.sh
@@ -4,18 +4,66 @@ InputTemplate=./template.yml
 BuiltTemplate=./.aws-sam/build/template.yaml
 DefaultLambda="RequestProductDetailsFunction"
 
-# run unit tests
-echo 'Unit Tests - generate signatures'
-ruby tests/unit/tc_request_product_details.rb --name /test_generate_signature*/
-echo 'Unit Tests - create query params'
-ruby tests/unit/tc_request_product_details.rb --name /test_create_query_params*/
+run_unit_tests() {
+  # run unit tests
+  echo 'Unit Tests - generate signatures'
+  ruby tests/unit/tc_request_product_details.rb --name /test_generate_signature*/
+  echo 'Unit Tests - create query params'
+  ruby tests/unit/tc_request_product_details.rb --name /test_create_query_params*/
+}
 
-# run local simulation of lambdas
+run_lambda_locally() {
+    sam local invoke -e events/SchmackosEvent.json $1
+}
+
+run_default() {
+  run_unit_tests
+  run_lambda_locally
+}
+
+POSITIONAL=()
+while [[ $# -gt 0 ]]
+do
+key="$1"
+
+case $key in
+    -u|--unit-test)
+    run_unit_tests
+    exit 0
+    shift # past argument
+    shift # past value
+    ;;
+    -l|--local-invoke)
+    run_lambda_locally
+    exit 0
+    shift # past argument
+    shift # past value
+    ;;
+    -a|--api)
+    sam local start-api
+    exit 0
+    shift # past argument
+    shift # past value
+    ;;
+    --default)
+    DEFAULT=YES
+    run_default
+    shift # past argument
+    ;;
+    *)    # unknown option
+    POSITIONAL+=("$1") # save it in an array for later
+    shift # past argument
+    ;;
+esac
+done
+set -- "${POSITIONAL[@]}" # restore positional parameters
+
+run local simulation of lambdas
 if [ -z "$1" ]
 then
   echo "no lambda function name supplied. Running default lambda: ${DefaultLambda}"
-  sam local invoke -e events/SchmackosEvent.json $DefaultLambda
+  run_lambda_locally $DefaultLambda
 else
   echo "Running lambda deployment test locally for function: $1"
-  sam local invoke -e events/SchmackosEvent.json $1
+  run_lambda_locally $1
 fi
